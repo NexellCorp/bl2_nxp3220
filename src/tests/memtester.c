38d32d7f9e3116fb235c6980f50ea39e215274b9
Deokjin Lee <truevirtue@nexell.co.kr>
2018-08-07 17:19:36 +0900

    add the licenses to the created files
    This patch adds a license that indicates the
    source to the created files.
    
    Change-Id: I478f3e68c10a2a502ac6f0dd1cb25b0a77fdd0b1
    Signed-off-by: Deokjin Lee <truevirtue@nexell.co.kr>

--- src/tests/memtester.c
+++ src/tests/memtester.c
@@ -1,4 +1,17 @@
 /*
+ * memtester version 4
+ *
+ * Very simple but very effective user-space memory tester.
+ * Originally by Simon Kirby <sim@stormix.com> <sim@neato.org>
+ * Version 2 by Charles Cazabon <charlesc-memtester@pyropus.ca>
+ * Version 3 not publicly released.
+ * Version 4 rewrite:
+ * Copyright (C) 2004-2012 Charles Cazabon <charlesc-memtester@pyropus.ca>
+ * Licensed under the terms of the GNU General Public License version 2 (only).
+ * See the file COPYING for details.
+ *
+ */
+/*
  * Copyright (C) 2018  Nexell Co., Ltd.
  * Author: deoks <truevirtue@nexell.co.kr>
  *
