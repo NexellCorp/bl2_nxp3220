6b63df49075fb45db907053fa51ec5d57e4c613b
Deokjin Lee <truevirtue@nexell.co.kr>
2018-08-10 14:48:34 +0900

    add the annotations to platform information
    This patch annotated the platform delivery structure.
    
    Change-Id: I245b99e240ddaf20b7adcda4cfd28c30e5c26519
    Signed-off-by: Deokjin Lee <truevirtue@nexell.co.kr>

--- src/drivers/plat_load.h
+++ src/drivers/plat_load.h
@@ -15,10 +15,12 @@
 #define __PLAT_LOAD_H__
 
 struct platform_info {
+	/* @brief: boot loader level x information */
 	unsigned int is_loadmark;
 	unsigned int s_dev_addr;
 	unsigned int n_dev_addr;
 	unsigned int sf_dev_addr;
+	/* @brief: Memory Function */
 	void (*ensr_func)(void);
 	void (*exsr_func)(void);
 };
